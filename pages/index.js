ac1d60e81105270ee996032a9124940af576b3ed
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-11-25 01:03:38 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { getData } from '../utils/getData';
-import { Layout, Menu, Button, Avatar, Carousel } from 'antd';
+import { Layout, Menu, Button, Avatar, Carousel, Typography } from 'antd';
 import css from 'styled-jsx/css';
+
+const { Title, Paragraph } = Typography;
 
 const { className, styles } = css.resolve`
   .ant-menu-dark.ant-menu-horizontal {
@@ -89,7 +91,6 @@ const Home = ({ name }) => {
 
               color: 'rgba(255, 255, 255, 1)',
               cursor: 'pointer',
-              marginRight: '15px',
             }}
           />
           {name ? (
@@ -99,13 +100,14 @@ const Home = ({ name }) => {
                 backgroundSize: 'cover',
                 verticalAlign: 'middle',
                 cursor: 'pointer',
+                marginLeft: '15px',
               }}
               size="large"
               gap={1}
             />
           ) : (
             <Link href="/login">
-              <Button type="primary" shape="round" size="middle" danger>
+              <Button type="link" shape="round" size="middle" danger>
                 Đăng nhập
               </Button>
             </Link>
@@ -141,7 +143,43 @@ const Home = ({ name }) => {
               draggable="false"
             />
           </div>
+          <div className="slide__image">
+            <Image
+              src="/images/slide-4.jpg"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              draggable="false"
+            />
+          </div>
         </Carousel>
+        <section className="slogan">
+          <div>
+            <Title level={1} style={{ color: '#40A9FF' }}>
+              Redefine yourself
+            </Title>
+            <Title level={2} style={{ color: 'rgba(0, 0, 0, 0.5)' }}>
+              Học tiếng Anh theo cách của bạn
+            </Title>
+            <Button type="primary" size="large" style={{ margin: '30px 0' }}>
+              Bắt đầu học
+            </Button>
+          </div>
+        </section>
+        <section className="divider">
+          <img src="/images/divider.png" />
+        </section>
+        <section className="why">
+          <div className="title">
+            <Title level={2}>Tại sao bạn nên học tại ETraining?</Title>
+            <Paragraph style={{ color: 'rgba(0, 0,0, 0.5)' }}>
+              ETraining được xây dựng để giúp bạn hệ thống lại toàn bộ từ vựng
+              tiếng Anh mà bạn đã học qua flashcard và nhắc nhở bạn học một từ
+              nào đó nhiều lần nếu bạn hay quên nó.
+            </Paragraph>
+          </div>
+          <div></div>
+        </section>
       </Content>
       <Footer></Footer>
       {styles}
@@ -159,6 +197,36 @@ const Home = ({ name }) => {
           .slide__image {
             height: 600px;
             position: relative;
+          }
+          .slogan {
+            padding: 4rem 1.5rem 1.5rem 1.5rem;
+            display: flex;
+            justify-content: center;
+            background: #fff;
+          }
+
+          .slogan div {
+            text-align: center;
+          }
+          .divider {
+            background: #fff;
+            padding-bottom: 30px;
+          }
+
+          .divider img {
+            height: 60px;
+            margin: 0 auto;
+            display: block;
+          }
+          .why {
+            padding: 15px;
+            display: flex;
+            justify-content: center;
+          }
+
+          .title {
+            text-align: center;
+            max-width: 1140px;
           }
         `}
       </style>
