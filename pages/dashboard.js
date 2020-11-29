362aaa05b9f4a7666de7d347a9803819e718ab6e
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-11-29 20:44:55 +0700

    update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,12 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState, memo } from 'react';
+import React, { useEffect, memo } from 'react';
 import Layout from '../components/layout';
 
 import { useValidated } from '../hooks';
 
 const Dashboard = () => {
   const router = useRouter();
-  const { loggedOut, data } = useValidated();
-  const [name, setName] = useState(null);
-  const [username, setUsername] = useState(null);
+  const { loggedOut } = useValidated();
 
   useEffect(() => {
     if (loggedOut) {
@@ -17,18 +15,11 @@ const Dashboard = () => {
   }, [loggedOut]);
 
   useEffect(() => {
-    if (data) {
-      setName(data?.name);
-      setUsername(data?.username);
-    }
-  }, [data]);
-
-  useEffect(() => {
     router.prefetch('/login');
   });
 
   return (
-    <Layout name={name} username={username} title="Bảng điều khiển | ETraining">
+    <Layout title="Bảng điều khiển | ETraining">
       <div>
         <h1>Không có gì đâu mà vào.</h1>
         <h1>Không có gì đâu mà vào.</h1>
